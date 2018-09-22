cda84fed06e7dfe3648d293908a4d5d3b6079e40
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-22 15:16:57 +0700

    initial project, wrote migrations and seeders

---
+++ database/migrations/2018_09_22_070010_create_users_transactions_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUsersTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users_transactions', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('from_user_id');
+            $table->unsignedInteger('to_user_id');
+            $table->unsignedDecimal('amount');
+            $table->unsignedTinyInteger('status_id');
+            $table->date('scheduled_time');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users_transactions');
+    }
+}
