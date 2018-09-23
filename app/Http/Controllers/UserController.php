7ad9b583fc4c9ea7b8317871ad8e90de794494c4
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-24 00:40:59 +0700

    added methods index and show to UserController and created Api UserController

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
@@ -15,16 +16,40 @@ class UserController extends Controller
      */
     public function index(User $user)
     {
-        $users = $user->all();
-        return response(json_encode($users));
+        $users = DB::table('users as senders')->leftJoin('users_transactions', function ($join) {
+            $join->on('senders.id', '=', 'users_transactions.from_user_id')
+                ->whereRaw('users_transactions.id = (select us.id from users_transactions us
+                where us.from_user_id = senders.id order by us.id desc limit 1)');
+        })->leftJoin('users as receivers', function ($join) {
+            $join->on('receivers.id', '=', 'users_transactions.to_user_id');
+        })->select(
+            'senders.id as id',
+            'senders.first_name as senderFirstName',
+            'senders.last_name as senderLastName',
+            'receivers.first_name as receiverFirstName',
+            'receivers.last_name as receiverLastName',
+            'users_transactions.amount',
+            'users_transactions.scheduled_time'
+
+
+        )->get();
+        return view('users', ['users' => $users]);
     }
 
-
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
 
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
@@ -41,15 +66,29 @@ class UserController extends Controller
     public function show($id)
     {
         $user = User::find($id);
-        return response(json_encode($user));
+        $otherUsers = User::where('id', '<>', $user->id)->get();
+        return view('user', [
+            'user' => $user,
+            'other_users' => $otherUsers
+        ]);
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
 
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
+     * @param  \Illuminate\Http\Request $request
+     * @param  int $id
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, $id)
@@ -57,4 +96,14 @@ class UserController extends Controller
         //
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
 }
