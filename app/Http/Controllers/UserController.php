ce3756c3e1bfba238e85329bbe772b4ac3be6d91
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-25 00:48:22 +0700

    deleted odd methods

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,18 +3,17 @@
 namespace App\Http\Controllers;
 
 use App\User;
-use Illuminate\Http\Request;
+use App\UserTransaction;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
 {
     /**
-     * Display a listing of the resource.
+     * Метод отображает всех пользователей, и информацию об их последней транзакции
      *
-     * @param User $user
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
-    public function index(User $user)
+    public function index()
     {
         $users = DB::table('users as senders')->leftJoin('users_transactions', function ($join) {
             $join->on('senders.id', '=', 'users_transactions.from_user_id')
@@ -29,81 +28,28 @@ class UserController extends Controller
             'receivers.first_name as receiverFirstName',
             'receivers.last_name as receiverLastName',
             'users_transactions.amount',
-            'users_transactions.scheduled_time'
-
-
+            'users_transactions.scheduled_time',
+            'users_transactions.status_id'
         )->get();
         return view('users', ['users' => $users]);
     }
 
     /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
+     * Отображаем текущего пользователя и пользователей которым можно совершить перевод
+     * и все транзакции текущего пользователя
      *
      * @param  int $id
-     * @return \Illuminate\Http\Response
+     * @return \Illuminate\View\View
      */
     public function show($id)
     {
         $user = User::find($id);
         $otherUsers = User::where('id', '<>', $user->id)->get();
+        $userTransactions = UserTransaction::where('from_user_id', $user->id)->get();
         return view('user', [
             'user' => $user,
-            'other_users' => $otherUsers
+            'other_users' => $otherUsers,
+            'transactions' => $userTransactions
         ]);
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request $request
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        //
     }
 }
