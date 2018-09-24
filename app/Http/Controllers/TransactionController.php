609b8d82db2bd809432fe8cac421e8d1e90ac31c
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-25 00:50:18 +0700

    deleted odd methods, added checking balance in method 'store', added new methods validateTransactionFormFields and destroy

--- app/Http/Controllers/TransactionController.php
+++ app/Http/Controllers/TransactionController.php
@@ -2,44 +2,45 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\UserTransaction;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class TransactionController extends Controller
 {
     /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function index()
-    {
-        //
-    }
-
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-
-    }
-
-    /**
-     * Store a newly created resource in storage.
+     * Метод добавления новой запланированной транзакции
      *
      * @param  \Illuminate\Http\Request $request
      * @param UserTransaction $userTransaction
+     * @param User $user
      * @return \Illuminate\Http\RedirectResponse
      */
-    public function store(Request $request, UserTransaction $userTransaction)
+    public function store(Request $request, UserTransaction $userTransaction, User $user)
     {
         $amount = $request->input('amount');
-        $toUserId = $request->input('toUser');
-        $fromUserId = $request->input('from_user_id');
+        $toUserId = $request->input('receiverId');
+        $fromUserId = $request->input('senderId');
         $dateTime = $request->input('dateTime');
+
+        if (($amount * 100) % 50 != 0) {
+            return redirect()->back()->with('danger', 'Сумма не кратна 50 копейкам');
+        }
+
+        $this->validateTransactionFormFields($request);
+
+        $balance = $user->find($fromUserId)->balance;
+
+        //Сумма списаний
+        $amountOfWriteOffs = $userTransaction->select(DB::raw('SUM(amount) as amountOfWriteOffs'))
+            ->where([['status_id', 3], ['from_user_id', $fromUserId]])->first()->amountOfWriteOffs;
+
+        $userTempBalance = $balance - $amountOfWriteOffs;
+
+        if ($userTempBalance < $amount) {
+            return redirect()->back()->with('danger', 'Недостаточно средств, ваш остаток ' . $userTempBalance . '₽');
+        }
 
         $userTransaction->create([
             'from_user_id' => $fromUserId,
@@ -48,52 +49,42 @@ class TransactionController extends Controller
             'scheduled_time' => $dateTime,
             'status_id' => 3
         ]);
-        $message = 'Транзакция успешно запланирована на '. $dateTime . '!';
+
+        $message = 'Транзакция успешно запланирована на ' . $dateTime . '!';
         return redirect()->back()->with('success', $message);
     }
 
     /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Метод валидаци полей формы
+     * @param Request $request
      */
-    public function show($id)
+    private function validateTransactionFormFields(Request $request)
     {
-        //
+        $messages = [
+            'required' => 'Заполните обязательное поле :attribute',
+            'integer' => 'Число должно быть целым :attribute.',
+            'max' => 'Нельзя перевести больше 1000000.',
+            'date_format' => 'Неккоректная дата'
+        ];
+
+        $this->validate($request, [
+            'amount' => 'required|numeric|min:0.5|max:1000000',
+            'receiverId' => 'required|integer',
+            'senderId' => 'required|integer',
+            'dateTime' => 'required|date_format:"Y-m-d H:00:00"',
+        ], $messages);
     }
 
     /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
+     * Метод удаления транзакции
+     * @param $id
+     * @return \Illuminate\Http\RedirectResponse
      */
     public function destroy($id)
     {
-        //
+        $userTransaction = UserTransaction::find($id);
+        $userTransaction->delete();
+        return redirect()->back()->with('success', 'Транзакция отменена');
     }
+
 }
