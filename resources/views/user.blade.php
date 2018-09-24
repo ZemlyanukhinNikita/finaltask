9469ad07a352905f8475e00676104457f65b5fad
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-25 00:44:49 +0700

    added display errors and information about user`s transactions

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,61 +1,5 @@
 @include('layouts.app')
-<div class="col-3">
-    {{$user->first_name}}
-    {{$user->last_name}} <br>
-    Счет: {{$user->balance}}<br>
-
-    Перевести деньги:<br>
-    Кому:<br>
-</div>
-{!! Form::open(['action' => 'TransactionController@store']) !!}
-{!! Form::token() !!}
-<ul>
-
-    @foreach($other_users as $otherUser)
-        <div>
-            <label>
-                <input type="radio" value="{{ $otherUser->id }}" name="toUser" required="required"/>
-                {{ $otherUser->first_name }}
-                {{ $otherUser->last_name }}
-            </label>
-        </div>
-    @endforeach
-
-    <input type="hidden" name="from_user_id" value="{{$user->id}}">
-
-    Сумма перевода:
-    <div class="row">
-        <div class="col-sm-2">
-            <input type="number" required class="form-control" placeholder="Сумма" name="amount" value=0
-                   min="1"
-                   max="1000000">
-        </div>
-    </div>
-    <br>
-    Дата и время перевода:<br><br>
-
-    <div class="row">
-        <div class='col-sm-2'>
-            <input type='text' class="form-control" id='datetimepicker4' name="dateTime"/>
-        </div>
-        <script type="text/javascript">
-            $(function () {
-                $('#datetimepicker4').datetimepicker({
-                    locale: 'ru',
-                    minDate: new Date(),
-                    format: 'YYYY-MM-DD HH:00:00'
-                });
-            });
-        </script>
-    </div>
-
-    <br>
-    {{--Дата и время перевода:--}}
-    {{--<input type="datetime-local" value="00">--}}
-
-    <input type="submit" class="btn btn-primary" value="Отправить">
-</ul>
-{!! Form::close() !!}
+<a href="/users">Вернуться к пользователям</a>
 @foreach(['danger', 'success'] as $status)
     @if (session()->has($status))
         <div class="alert alert-{{$status}}">
@@ -63,3 +7,106 @@
         </div>
     @endif
 @endforeach
+
+@if ($errors->any())
+    <div class="alert alert-danger">
+        <ul>
+            @foreach ($errors->all() as $error)
+                <li>{{ $error }}</li>
+            @endforeach
+        </ul>
+    </div>
+@endif
+<div class="col-3 float-right">
+    @foreach($transactions as $transaction)
+        <div>
+            @if($transaction->status_id == 3)
+
+                <p class="alert-info">Запланированная транзакция:</p> Перевод
+                пользователю {{$transaction->receiver->first_name}}<br>
+                Сумма перевода: {{$transaction->amount}}<br>
+                Дата и время перевода: {{$transaction->scheduled_time}}
+                {!! Form::open(['method'=>'delete','action'=>'TransactionController@destroy', 'route' => ['transactions.destroy',$transaction->id]]) !!}
+                <input type="submit" class="btn btn-primary" value="Отменить">
+                {!! Form::close()!!}
+            @endif
+        </div>
+
+        <div>
+            @if($transaction->status_id == 1)
+                <p class="alert-success">Совершенная транзакция:</p>  Перевод
+                пользователю {{$transaction->receiver->first_name}}<br>
+                Сумма перевода: {{$transaction->amount}}<br>
+                Дата и время перевода: {{$transaction->scheduled_time}}
+            @endif
+        </div>
+
+        <div>
+            @if($transaction->status_id == 2)
+                <p class="alert-danger">Невыполненная транзакция:</p> Перевод
+                пользователю {{$transaction->receiver->first_name}}<br>
+                Сумма перевода: {{$transaction->amount}}<br>
+                Дата и время перевода: {{$transaction->scheduled_time}}
+            @endif
+        </div>
+    @endforeach
+</div>
+<div class="col-9 float-left">
+    {{$user->first_name}}
+    {{$user->last_name}} <br>
+    Баланс: {{$user->balance}}<br>
+
+    Перевести деньги:<br>
+    Кому:<br>
+
+
+    {!! Form::open(['action' => 'TransactionController@store']) !!}
+    @csrf
+
+    <ul>
+
+        @foreach($other_users as $otherUser)
+            <div>
+                <label>
+                    <input type="radio" value="{{ $otherUser->id }}" name="receiverId" required="required"/>
+                    {{ $otherUser->first_name }}
+                    {{ $otherUser->last_name }}
+                </label>
+            </div>
+        @endforeach
+
+        <input type="hidden" name="senderId" value="{{$user->id}}">
+
+        Сумма перевода:
+        <div class="row">
+            <div class="col-sm-2">
+                <input type="number" required class="form-control" placeholder="Сумма" name="amount" step="0.5"
+                       min="0.5"
+                       max="1000000">
+            </div>
+        </div>
+        <br>
+        Дата и время перевода:<br><br>
+
+        <div class="row">
+            <div class='col-sm-2'>
+                <input type='text' class="form-control" id='datetimepicker4' name="dateTime"/>
+            </div>
+            <script type="text/javascript">
+                $(function () {
+                    $('#datetimepicker4').datetimepicker({
+                        locale: 'ru',
+                        minDate: new Date(),
+                        format: 'YYYY-MM-DD HH:00:00',
+                    });
+                });
+            </script>
+        </div>
+
+        <br>
+
+        <input type="submit" class="btn btn-primary" value="Отправить">
+    </ul>
+    {!! Form::close() !!}
+
+</div>
--- resources/views/users.blade.php
+++ resources/views/users.blade.php
@@ -5,6 +5,11 @@
         <li><a href="/users/{{$user->id}}">{{$user->senderFirstName}} {{$user->senderLastName}}</a></li>
         @if($user->amount)
             Информация о последней транзакции:<br>
+            Статус транзакции:
+            @if($user->status_id == 1) Выполнена<br>
+            @elseif($user->status_id == 2) Невыполнена<br>
+            @else Ожидается перевод<br>
+            @endif
             Сумма перевода: {{$user->amount}} <br>
             Назначенное время: {{$user->scheduled_time}} <br>
             Кому: <br>
@@ -12,6 +17,5 @@
         @else Пользователь еще не совершал транзакций.
         @endif<br>
         @endforeach
-
     </ul>
 @endif
