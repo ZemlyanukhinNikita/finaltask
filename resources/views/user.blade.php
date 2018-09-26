7053c49da411d9ec107276ac296107e0455e446c
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-26 14:05:01 +0700

    makeup page

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,5 +1,4 @@
 @include('layouts.app')
-<a href="/users">Вернуться к пользователям</a>
 @foreach(['danger', 'success'] as $status)
 
     @if (session()->has($status))
@@ -18,96 +17,95 @@
         </ul>
     </div>
 @endif
-<div class="col-3 float-right">
-    @foreach($transfers as $transfer)
-        <div>
-            @if($transfer->status_id == 3)
+<div class="container">
+    <div class="row">
+        <div class="col-6">
+            <h4><a href="/users">Вернуться к пользователям</a></h4>
+            {{$user->first_name}}
+            {{$user->last_name}} <br>
+            Баланс: {{$user->balance}}<br>
 
-                <p class="alert-info">Запланированная транзакция:</p> Перевод
-                пользователю {{$transfer->receiver->first_name}}<br>
-                Сумма перевода: {{$transfer->amount}}<br>
-                Дата и время перевода: {{$transfer->scheduled_time}}
-                {!! Form::open(['method'=>'delete','action'=>'TransferController', 'route' => ['transfers.destroy',$transfer->id]]) !!}
-                <input type="submit" class="btn btn-primary" value="Отменить">
-                {!! Form::close()!!}
-            @endif
-        </div>
-
-        <div>
-            @if($transfer->status_id == 1)
-                <p class="alert-success">Совершенная транзакция:</p>  Перевод
-                пользователю {{$transfer->receiver->first_name}}<br>
-                Сумма перевода: {{$transfer->amount}}<br>
-                Дата и время перевода: {{$transfer->scheduled_time}}
-            @endif
-        </div>
-
-        <div>
-            @if($transfer->status_id == 2)
-                <p class="alert-danger">Невыполненная транзакция:</p> Перевод
-                пользователю {{$transfer->receiver->first_name}}<br>
-                Сумма перевода: {{$transfer->amount}}<br>
-                Дата и время перевода: {{$transfer->scheduled_time}}
-            @endif
-        </div>
-    @endforeach
-</div>
-<div class="col-9 float-left">
-    {{$user->first_name}}
-    {{$user->last_name}} <br>
-    Баланс: {{$user->balance}}<br>
-
-    Перевести деньги:<br>
-    Кому:<br>
+            Перевести деньги:<br>
+            Кому:<br>
 
 
-    {!! Form::open(['action' => 'TransferController@store']) !!}
-    @csrf
+            {!! Form::open(['action' => 'TransferController@store']) !!}
+            @csrf
 
-    <ul>
+            <ul>
 
-        @foreach($other_users as $otherUser)
-            <div>
-                <label>
-                    <input type="radio" value="{{ $otherUser->id }}" name="receiverId" required="required"/>
-                    {{ $otherUser->first_name }}
-                    {{ $otherUser->last_name }}
-                </label>
-            </div>
-        @endforeach
+                @foreach($other_users as $otherUser)
+                    <div>
+                        <label>
+                            <input type="radio" value="{{ $otherUser->id }}" name="receiverId" required="required"/>
+                            {{ $otherUser->first_name }}
+                            {{ $otherUser->last_name }}
+                        </label>
+                    </div>
+                @endforeach
 
-        <input type="hidden" name="senderId" value="{{$user->id}}">
+                <input type="hidden" name="senderId" value="{{$user->id}}">
 
-        Сумма перевода:
-        <div class="row">
-            <div class="col-sm-2">
-                <input type="number" required class="form-control" placeholder="Сумма" name="amount" step="0.5"
+                Сумма перевода:
+                <input style='width: 200px' type="number" required class="form-control" placeholder="Сумма" name="amount"
+                       step="0.5"
                        min="0.5"
                        max="1000000">
-            </div>
-        </div>
-        <br>
-        Дата и время перевода:<br><br>
+                <br>
+                Дата и время перевода:<br><br>
 
-        <div class="row">
-            <div class='col-sm-2'>
-                <input type='text' class="form-control" id='datetimepicker4' name="dateTime"/>
-            </div>
-            <script type="text/javascript">
-                $(function () {
-                    $('#datetimepicker4').datetimepicker({
-                        locale: 'ru',
-                        minDate: new Date(),
-                        format: 'YYYY-MM-DD HH:00:00',
+                <input style='width: 200px' type='text' class="form-control" id='datetimepicker4' name="dateTime"/>
+                <script type="text/javascript">
+                    $(function () {
+                        $('#datetimepicker4').datetimepicker({
+                            locale: 'ru',
+                            minDate: new Date(),
+                            format: 'YYYY-MM-DD HH:00:00',
+                        });
                     });
-                });
-            </script>
+                </script>
+
+                <br>
+
+                <input type="submit" class="btn btn-primary" value="Отправить">
+            </ul>
+            {!! Form::close() !!}
+
         </div>
+        <div class="col-6">
+            @foreach($transfers as $transfer)
+                <div>
+                    @if($transfer->status_id == 3)
 
-        <br>
+                        <p class="alert-info">Запланированная транзакция:</p> Перевод
+                        пользователю {{$transfer->receiver->first_name}}<br>
+                        Сумма перевода: {{$transfer->amount}}<br>
+                        Дата и время перевода: {{$transfer->scheduled_time}}
+                        {!! Form::open(['method'=>'delete','action'=>'TransferController', 'route' => ['transfers.destroy',$transfer->id]]) !!}
+                        <input type="submit" class="btn btn-primary" value="Отменить">
+                        {!! Form::close()!!}
+                    @endif
+                </div>
 
-        <input type="submit" class="btn btn-primary" value="Отправить">
-    </ul>
-    {!! Form::close() !!}
+                <div>
+                    @if($transfer->status_id == 1)
+                        <p class="alert-success">Совершенная транзакция:</p>  Перевод
+                        пользователю {{$transfer->receiver->first_name}}<br>
+                        Сумма перевода: {{$transfer->amount}}<br>
+                        Дата и время перевода: {{$transfer->scheduled_time}}
+                    @endif
+                </div>
 
+                <div>
+                    @if($transfer->status_id == 2)
+                        <p class="alert-danger">Невыполненная транзакция:</p> Перевод
+                        пользователю {{$transfer->receiver->first_name}}<br>
+                        Сумма перевода: {{$transfer->amount}}<br>
+                        Дата и время перевода: {{$transfer->scheduled_time}}
+                    @endif
+                </div>
+            @endforeach
+        </div>
+    </div>
+</div>
 </div>
