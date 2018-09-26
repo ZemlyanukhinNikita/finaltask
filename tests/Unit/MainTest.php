1ffdc836519ee44f2ab5af621a4281637a1d1992
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-26 19:20:14 +0700

    fixed tests

--- tests/Unit/MainTest.php
+++ tests/Unit/MainTest.php
@@ -31,66 +31,71 @@ class MainTest extends TestCase
     public function testCreateTransfers()
     {
         $dateTime = Carbon::now()->addHour();
-        $dateTime= $dateTime->format('Y-m-d H:00:00');
+        $dateTime = $dateTime->format('Y-m-d H:00:00');
         $this->json('POST', 'http://localhost:8080/transfers', [
-                //todo send csrf token
-                'senderId' => 1,
-                'receiverId' => 2,
-                'amount' => 1,
-                'dateTime' => $dateTime
+            'senderId' => 1,
+            'receiverId' => 2,
+            'amount' => 1,
+            'dateTime' => $dateTime
         ]);
-        $this->assertDatabaseHas('users_transfers', ['sender_id' => 1, 'receiver_id' => 2, 'amount' => 1, 'scheduled_time' => $dateTime]);
+        $this->assertDatabaseHas('users_transfers',
+            ['sender_id' => 1,
+                'receiver_id' => 2,
+                'amount' => 1,
+                'scheduled_time' => $dateTime
+            ]
+        );
     }
 
     public function testNotEnoughMoney()
     {
         $dateTime = Carbon::now()->addHour();
-        $dateTime= $dateTime->format('Y-m-d H:00:00');
-        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+        $dateTime = $dateTime->format('Y-m-d H:00:00');
+        $response = $this->json('POST', 'transfers', [
             'senderId' => 1,
             'receiverId' => 2,
             'amount' => 9999999,
             'dateTime' => $dateTime
         ]);
-        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertEquals(422, $response->getStatusCode());
     }
 
     public function testTheSameUsers()
     {
         $dateTime = Carbon::now()->addHour();
-        $dateTime= $dateTime->format('Y-m-d H:00:00');
-        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+        $dateTime = $dateTime->format('Y-m-d H:00:00');
+        $response = $this->json('POST', 'transfers', [
             'senderId' => 1,
             'receiverId' => 1,
             'amount' => 100,
             'dateTime' => $dateTime
         ]);
-        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertEquals(422, $response->getStatusCode());
     }
 
     public function testInvalidDate()
     {
         $dateTime = Carbon::now()->addHour();
-        $invalidDate= $dateTime->format('Y-m-d');
-        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+        $invalidDate = $dateTime->format('Y-m-d');
+        $response = $this->json('POST', 'transfers', [
             'senderId' => 1,
             'receiverId' => 2,
             'amount' => 100,
             'dateTime' => $invalidDate
         ]);
-        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertEquals(422, $response->getStatusCode());
     }
 
     public function testInvalidPastDate()
     {
         $dateTime = Carbon::now()->subHour();
-        $invalidPastDateTime= $dateTime->format('Y-m-d H:00:00');
-        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+        $invalidPastDateTime = $dateTime->format('Y-m-d H:00:00');
+        $response = $this->json('POST', 'transfers', [
             'senderId' => 1,
             'receiverId' => 2,
             'amount' => 100,
             'dateTime' => $invalidPastDateTime
         ]);
-        $this->assertEquals(404, $response->getStatusCode());
+        $this->assertEquals(422, $response->getStatusCode());
     }
 }
