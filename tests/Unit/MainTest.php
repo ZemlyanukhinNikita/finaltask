78f92ed49289cbca7268109b8f50c38d40fb722e
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-26 14:02:40 +0700

    added new tests

---
+++ tests/Unit/MainTest.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace Tests\Unit;
+
+use App\Services\TransactionService;
+use App\User;
+use App\UserTransfer;
+use Carbon\Carbon;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
+use Tests\TestCase;
+
+class MainTest extends TestCase
+{
+    use DatabaseTransactions;
+
+    private $transactionService;
+    private $userTransfer;
+    private $user;
+
+    protected function setUp()
+    {
+        $this->user = new User();
+        $this->userTransfer = new UserTransfer();
+        $this->transactionService = new TransactionService();
+
+        parent::setUp();
+    }
+
+    public function testCreateTransfers()
+    {
+        $dateTime = Carbon::now()->addHour();
+        $dateTime= $dateTime->format('Y-m-d H:00:00');
+        $this->json('POST', 'http://localhost:8080/transfers', [
+                //todo send csrf token
+                'senderId' => 1,
+                'receiverId' => 2,
+                'amount' => 1,
+                'dateTime' => $dateTime
+        ]);
+        $this->assertDatabaseHas('users_transfers', ['sender_id' => 1, 'receiver_id' => 2, 'amount' => 1, 'scheduled_time' => $dateTime]);
+    }
+
+    public function testNotEnoughMoney()
+    {
+        $dateTime = Carbon::now()->addHour();
+        $dateTime= $dateTime->format('Y-m-d H:00:00');
+        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+            'senderId' => 1,
+            'receiverId' => 2,
+            'amount' => 9999999,
+            'dateTime' => $dateTime
+        ]);
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testTheSameUsers()
+    {
+        $dateTime = Carbon::now()->addHour();
+        $dateTime= $dateTime->format('Y-m-d H:00:00');
+        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+            'senderId' => 1,
+            'receiverId' => 1,
+            'amount' => 100,
+            'dateTime' => $dateTime
+        ]);
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testInvalidDate()
+    {
+        $dateTime = Carbon::now()->addHour();
+        $invalidDate= $dateTime->format('Y-m-d');
+        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+            'senderId' => 1,
+            'receiverId' => 2,
+            'amount' => 100,
+            'dateTime' => $invalidDate
+        ]);
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+
+    public function testInvalidPastDate()
+    {
+        $dateTime = Carbon::now()->subHour();
+        $invalidPastDateTime= $dateTime->format('Y-m-d H:00:00');
+        $response = $this->json('POST', 'http://localhost:8080/transaction', [
+            'senderId' => 1,
+            'receiverId' => 2,
+            'amount' => 100,
+            'dateTime' => $invalidPastDateTime
+        ]);
+        $this->assertEquals(404, $response->getStatusCode());
+    }
+}
--- tests/Unit/TransactionServiceTest.php
+++ tests/Unit/TransactionServiceTest.php
@@ -6,82 +6,36 @@ use App\Services\TransactionService;
 use App\User;
 use App\UserTransfer;
 use Carbon\Carbon;
-use GuzzleHttp\Client;
+use Illuminate\Foundation\Testing\DatabaseTransactions;
 use Tests\TestCase;
 
 class TransactionServiceTest extends TestCase
 {
-    private $sender;
-    private $receiver;
+    use DatabaseTransactions;
+
     private $transactionService;
     private $userTransfer;
     private $user;
-    private $request;
-    private $client;
-    private $dateTime;
 
     protected function setUp()
     {
-        $this->client = new Client();
         $this->user = new User();
         $this->userTransfer = new UserTransfer();
-
-        $this->dateTime = Carbon::now()->addHours(1);
-        $this->dateTime = $this->dateTime->format('Y-m-d H:00:00');
-
         $this->transactionService = new TransactionService();
-//        $this->sender = [
-//            'id' => 999,
-//            'first_name' => 'Тест',
-//            'last_name' => 'Тестовый',
-//            'balance' => 10000
-//        ];
-//
-//        $this->receiver = [
-//            'id' => 1000,
-//            'first_name' => 'Тест',
-//            'last_name' => 'Тестовый',
-//            'balance' => 0
-//        ];
         parent::setUp();
     }
 
-    public function testCreateTransactions()
+    public function testExecuteTransaction()
     {
+        $dateTime = Carbon::now()->subHour();
+        $dateTime = $dateTime->format('Y-m-d H:00:00');
 
-        $this->client->request('POST', 'http://localhost:8080/transfers', [
-            //todo send csrf token
-//                'headers'  => [
-//                    'X-CSRF-TOKEN' => 'eyJpdiI6IjBoNWZzVGFHRHE1NHYyWVhcLzB6M0FRPT0iLCJ2YWx1ZSI6IkdTSk5adUhxcWNleHc0UzNCYWFOaGNkRW9KZ1VSOW9ldk1NWWpcL3d3S3JvZUxWR0QyTENieXhacjFsdWJ5aXhwIiwibWFjIjoiZjZkNzYzZjAxZTIwODAzNTAzZmM3N2Y3MWY1ZTQ4YjNhMDFmNThmMmI1MDYzMjY2NTczMGExZWM5NDg0YTU3OCJ9'
-//                ],
-            'form_params' => [
-                'senderId' => 1,
-                'receiverId' => 2,
-                'amount' => 100,
-                'dateTime' => $this->dateTime
-            ]
-        ]);
-
-        $this->assertDatabaseHas('users_transfers', ['sender_id' => 1, 'receiver_id' => 2, 'amount' => 100, 'scheduled_time' => $this->dateTime]);
-
-        $this->userTransfer->where([['sender_id', 1], ['receiver_id', 2], ['amount', 100], ['scheduled_time', $this->dateTime]])->delete();
-        $this->assertDatabaseMissing('users_transfers', ['sender_id' => 1, 'receiver_id' => 2, 'amount' => 100, 'scheduled_time' => $this->dateTime]);
-    }
-
-    public function testCheckTransaction()
-    {
-
-        $this->client->request('POST', 'http://localhost:8080/transfers', [
-            //todo send csrf token
-//                'headers'  => [
-//                    'X-CSRF-TOKEN' => 'eyJpdiI6IjBoNWZzVGFHRHE1NHYyWVhcLzB6M0FRPT0iLCJ2YWx1ZSI6IkdTSk5adUhxcWNleHc0UzNCYWFOaGNkRW9KZ1VSOW9ldk1NWWpcL3d3S3JvZUxWR0QyTENieXhacjFsdWJ5aXhwIiwibWFjIjoiZjZkNzYzZjAxZTIwODAzNTAzZmM3N2Y3MWY1ZTQ4YjNhMDFmNThmMmI1MDYzMjY2NTczMGExZWM5NDg0YTU3OCJ9'
-//                ],
-            'form_params' => [
-                'senderId' => 1,
-                'receiverId' => 2,
-                'amount' => 100,
-                'dateTime' => $this->dateTime
-            ]
+        $this->userTransfer->create([
+            'sender_id' => 1,
+            'receiver_id' => 2,
+            'amount' => 100,
+            'status_id' => 3,
+            'scheduled_time' => $dateTime,
         ]);
 
         $userSender = $this->user->find(1);
@@ -94,11 +48,5 @@ class TransactionServiceTest extends TestCase
 
         $this->assertEquals($userSender->balance - 100, $userSenderAfterTransaction->balance);
         $this->assertEquals($userReceiver->balance + 100, $userReceiverAfterTransaction->balance);
-
-        $userSenderAfterTransaction->balance += 100;
-        $userReceiverAfterTransaction->balance -= 100;
-
-        $userSenderAfterTransaction->save();
-        $userReceiverAfterTransaction->save();
     }
 }
