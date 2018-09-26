bea318048ff2b04894ae0dc78e1b7173df80ba97
ZemlyanukhinNikita <NAKEdeveloper7676>
2018-09-26 19:19:50 +0700

    code style

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use App\User;
 use App\UserTransfer;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 
 class UserController extends Controller
